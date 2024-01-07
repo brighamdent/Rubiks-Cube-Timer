eb217ee8f1c6eceeaf2610d09fac5b57c77d67a9
brighamdent <brighamdent@gmail.com>
2024-01-06 20:44:45 -0600

    tweak

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Rubiks-Cube-Timer/',
+  base: '/Rubiks-Cube-Timer/timer',
   plugins: [react()],
 })
