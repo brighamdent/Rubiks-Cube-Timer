00011ff984dcfc3bdc449f9dd607cee066434714
brighamdent <brighamdent@gmail.com>
2024-05-08 10:33:08 -0600

    changing domain

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://brighamdent.github.io/Rubiks-Cube-Timer",
+  base: "https://cubequick.com",
   plugins: [react()],
 });
